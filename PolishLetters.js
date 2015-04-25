57fc64f956b95ff467921350cd6401772ac971d1
zaxad23 <adamek23wp.pl@wp.pl>
2015-04-25 23:12:17 +0200

    upper case

--- PolishLetters.js
+++ PolishLetters.js
@@ -14,15 +14,15 @@ String.prototype.replacePolishLetters = function(){
    
    // zamiana 
    
-   result=result.replace(/ę/g,"e");
-   result=result.replace(/ó/g,"o");
-   result=result.replace(/ł/g,"l");
-   result=result.replace(/ą/g,"a");
-   result=result.replace(/ń/g,"n");
-   result=result.replace(/ś/g,"s");
-   result=result.replace(/ć/g,"c");
-   result=result.replace(/ż/g,"z");
-   result=result.replace(/ź/g,"z");
+   result=result.replace(/ę/g,"e"); result=result.replace(/Ę/g,"E");
+   result=result.replace(/ó/g,"o"); result=result.replace(/Ó/g,"O");
+   result=result.replace(/ł/g,"l"); result=result.replace(/Ł/g,"L");
+   result=result.replace(/ą/g,"a"); result=result.replace(/Ą/g,"A");
+   result=result.replace(/ń/g,"n"); result=result.replace(/Ń/g,"N");
+   result=result.replace(/ś/g,"s"); result=result.replace(/Ś/g,"S");
+   result=result.replace(/ć/g,"c"); result=result.replace(/Ć/g,"C");
+   result=result.replace(/ż/g,"z"); result=result.replace(/Ż/g,"Z");
+   result=result.replace(/ź/g,"z"); result=result.replace(/Ź/g,"Z");
    
    return result; //zwrócenie
 }
